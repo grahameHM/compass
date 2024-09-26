c5167605ef454d6433e17945f727cc556054f3f1
grahameHM <49197934+grahameHM@users.noreply.github.com>
2024-09-26 15:48:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let Degrees = 0
+basic.forever(function () {
+    Degrees = input.compassHeading()
+    if (Degrees < 45) {
+        basic.showString("N")
+    } else {
+        if (Degrees < 135) {
+            basic.showString("E")
+        } else {
+            if (Degrees < 225) {
+                basic.showString("S")
+            } else {
+                if (Degrees < 315) {
+                    basic.showString("W")
+                }
+                basic.showString("N")
+            }
+        }
+    }
+})
